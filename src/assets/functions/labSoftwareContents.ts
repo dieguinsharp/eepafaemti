e4a83551c4780a335dcd92340cfa5c85ffc4befe
dieguito <dieguinsharp@gmail.com>
2025-08-04 17:31:12 -0700

    added new lab software classes

--- src/assets/functions/labSoftwareContents.ts
+++ src/assets/functions/labSoftwareContents.ts
@@ -1,6 +1,7 @@
 import {Content} from "../../common/Content.ts";
 
 import labSoftware01 from '../emti3/labsoftware.pdf';
+import labSoftware02 from '../emti3/labsoftware0508.pptx'
 
 // gestao do tempo area
 
@@ -14,6 +15,10 @@ function makeContentObjects() : Content[] {
         {
             title: defaultName + 'Primeira Aula',
             path: labSoftware01
+        },
+        {
+            title: defaultName + 'Primeira Aula - 3o Bimestre - 05/08',
+            path: labSoftware02
         }
     ]
 }
