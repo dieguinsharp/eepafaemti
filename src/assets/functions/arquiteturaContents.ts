55f64cd30ffab47168ef0a2a13dcfc6a10bdfad8
dieguito <dieguinsharp@gmail.com>
2025-10-05 16:16:03 -0700

    added architecture classes

--- src/assets/functions/arquiteturaContents.ts
+++ src/assets/functions/arquiteturaContents.ts
@@ -1,6 +1,7 @@
 import {Content} from "../../common/Content.ts";
 
 import arquitetura01 from '../emti2/arquitetura.pdf';
+import arquitetura4b from '../emti2/arquitetura4b.pdf';
 
 // gestao do tempo area
 
@@ -14,6 +15,10 @@ function makeContentObjects() : Content[] {
         {
             title: defaultName + 'Primeira Aula',
             path: arquitetura01
+        },
+        {
+            title: defaultName + 'Arquitetura 4 Bimestre',
+            path: arquitetura4b
         }
     ]
 }
